1adfb560a1619acb4e8790d80edb2441d4fd70b9
OscarAcademlo <oscarns@gmail.com>
2024-07-20 14:56:44 -0300

    correccion celular

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ function envio() {
         return;
     }
 
-    let mensaje = `https://api.whatsapp.com/send?phone=542944651381&text=Quiero%20un%20turno%20con%20el%20%20Medico%20${medico.value}+%0AEn%20el%20horario%20+${horario.value}%0A+Mis%20datos%20personales%20son:%0A+Nombre:%20${nombre.value}%0A+Apellido:%20+${apellido.value}%0A+DNI:%20+${dni.value}%0A+Obra%20Social:%20+${os.value}%0A+Email:%20+${email.value}%0A+Telefono:%20+${telefono.value}`;
+    let mensaje = `https://api.whatsapp.com/send?phone=+5492944682681&text=Quiero%20un%20turno%20con%20el%20%20Medico%20${medico.value}+%0AEn%20el%20horario%20+${horario.value}%0A+Mis%20datos%20personales%20son:%0A+Nombre:%20${nombre.value}%0A+Apellido:%20+${apellido.value}%0A+DNI:%20+${dni.value}%0A+Obra%20Social:%20+${os.value}%0A+Email:%20+${email.value}%0A+Telefono:%20+${telefono.value}`;
     enviar.href = mensaje;
     localStorage.clear(); // Clear the data after successful submission
 }
